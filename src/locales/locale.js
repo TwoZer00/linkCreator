eb538ea6a6062c4ad7675fe0bf0ecd45d3bca091
Leo V <leobardo@twozer00.dev>
2026-01-23 13:49:06 -0700

    refactor: simplify language label retrieval logic in locale.js

--- src/locales/locale.js
+++ src/locales/locale.js
@@ -5,12 +5,7 @@ import { navigatorLang } from '../utils/navigator';
 const labels = { en, es }
 
 export const label = (key) => {
-    if (!labels[navigatorLang][key]) {
-        return key;
-    }
-    if (!labels[navigatorLang]) {
-        return labels["en"][key]
-    }
-    return labels[navigatorLang][key]
+    const currentLang = labels[navigatorLang] || labels['en'];
+    return currentLang[key] || labels['en'][key] || key;
 };
 
